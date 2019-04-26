153ecc79355c52d5d87011925d7bbab1937649c1
smallgod <davies.mugume@gmail.com>
2019-04-26 17:00:15 +0300

    URA case Management repo setup - 1st commit

---
+++ src/main/java/com/ura/casemgt/configuration/ApplicationEntry.java
@@ -0,0 +1,8 @@
+package com.ura.casemgt.configuration;
+
+public class ApplicationEntry {
+
+    public static void main(String [] args){
+        System.out.println("Entry application from here....");
+    }
+}
