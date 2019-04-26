f0921e517568b17c224814f04bf9623fde968008
smallgod <davies.mugume@gmail.com>
2019-04-26 17:23:10 +0300

    setup daemon context

--- src/main/java/com/ura/casemgt/configuration/ApplicationEntry.java
+++ src/main/java/com/ura/casemgt/configuration/ApplicationEntry.java
@@ -1,8 +1,31 @@
 package com.ura.casemgt.configuration;
 
-public class ApplicationEntry {
+import org.apache.commons.daemon.Daemon;
+import org.apache.commons.daemon.DaemonContext;
 
-    public static void main(String [] args){
-        System.out.println("Entry application from here....");
+public class ApplicationEntry implements Daemon {
+
+    public static void main(String[] args) {
+        System.out.println("Jesus Christ is LORD! Method here for just..");
+    }
+
+    @Override
+    public void init(DaemonContext context) {
+
+    }
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+
+    @Override
+    public void destroy() {
+
     }
 }
